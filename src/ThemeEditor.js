ca7964f705392855e7a0f8f1115af3ecbfd2a056
Lincoln Anderson <landerson@352inc.com>
2019-03-03 21:43:27 -0500

    Add categories.

--- src/ThemeEditor.js
+++ src/ThemeEditor.js
@@ -1,6 +1,6 @@
 import React, { Fragment } from 'react';
 import { compose, lifecycle, withHandlers, withPropsOnChange, withState } from 'recompose';
-import { find, map, startsWith } from 'lodash';
+import { each, find, map, startsWith } from 'lodash';
 import Textarea from 'react-textarea-autosize';
 import { FirestoreCollection, withFirestore } from 'react-firestore';
 import cn from 'classnames';
@@ -10,11 +10,41 @@ import flattenVariables from './flattenVariables';
 import mergeVariables from './mergeVariables';
 import withLoadingSpinner from './withLoadingSpinner';
 
+const categoryNames = [
+  'button-large',
+  'button-small',
+  'button-link',
+  'button-text',
+  'button-danger',
+  'button-secondary',
+  'button-primary',
+  'button-default',
+  'button',
+];
+
 const ThemeVariablesEditor = compose(
   withLoadingSpinner,
   withPropsOnChange(['data'], ({ data }) => ({
     variables: mergeVariables(themeDefaults.variables, data),
   })),
+  withPropsOnChange(['variables'], ({ variables }) => {
+    const categories = map(categoryNames, (categoryName) => ({
+      name: categoryName,
+      variables: [],
+    }));
+    each(variables, (variable) => {
+      const category = find(categories, (category) => {
+        return startsWith(variable.name, `@${category.name}`);
+      });
+      if (category) {
+        category.variables.push(variable);
+      }
+    });
+    categories.reverse();
+    return {
+      categories,
+    };
+  }),
   withFirestore,
   lifecycle({
     componentDidMount() {
@@ -51,39 +81,46 @@ const ThemeVariablesEditor = compose(
         });
     },
   }),
-)(({ isLoading, data, handleVariableChange, variables }) => {
+)(({ categories, isLoading, data, handleVariableChange }) => {
   // console.log(variables);
   return (
-    <Fragment>
-      {map(variables, ({ isChanged, isCustom, name, value }, key) => (
-        <div key={key}>
-          <label className="uk-form-label">{ name }</label>
-          <div className="uk-form-controls">
-            {startsWith(name, '@hook-') ? (
-              <div className="uk-margin-small-bottom">
+    <ul data-uk-accordion>
+      {map(categories, ({ name, variables }) => (
+        <li>
+          <a className="uk-accordion-title" href="#">{ name }</a>
+          <div className="uk-accordion-content">
+            {map(variables, ({ isChanged, isCustom, name, value }, key) => (
+              <div key={key} className="uk-form-stacked">
+                <label className="uk-form-label">{ name }</label>
+                <div className="uk-form-controls">
+                  {startsWith(name, '@hook-') ? (
+                    <div className="uk-margin-small-bottom">
                 <Textarea
                   className="uk-textarea uk-form-small resize-none"
                   id={name}
                   defaultValue={value}
                   onChange={handleVariableChange}
                 />
+                    </div>
+                  ) : (
+                    <div className="uk-position-relative uk-margin-small-bottom">
+                      {isCustom && <button className="uk-form-icon uk-form-icon-flip" uk-icon="icon: trash" type="button"></button>}
+                      {isChanged && <button className="uk-form-icon uk-form-icon-flip" uk-icon="icon: refresh" type="button"></button>}
+                      <input
+                        className={cn('uk-input', 'uk-form-small', { 'uk-form-success': isChanged })}
+                        id={name}
+                        defaultValue={value}
+                        onChange={handleVariableChange}
+                      />
+                    </div>
+                  )}
+                </div>
               </div>
-            ) : (
-              <div className="uk-position-relative uk-margin-small-bottom">
-                {isCustom && <button className="uk-form-icon uk-form-icon-flip" uk-icon="icon: trash" type="button"></button>}
-                {isChanged && <button className="uk-form-icon uk-form-icon-flip" uk-icon="icon: refresh" type="button"></button>}
-                <input
-                  className={cn('uk-input', 'uk-form-small', { 'uk-form-success': isChanged })}
-                  id={name}
-                  defaultValue={value}
-                  onChange={handleVariableChange}
-                />
-              </div>
-            )}
+            ))}
           </div>
-        </div>
+        </li>
       ))}
-    </Fragment>
+    </ul>
   );
 });
 
@@ -106,15 +143,13 @@ const ThemeEditor = compose(
             {/*<li><a href="#" uk-icon="icon: menu"></a></li>*/}
             {/*<li><a href="#" uk-icon="icon: menu"></a></li>*/}
             {/*</ul>*/}
-            <div className="uk-form-stacked">
-              <ThemeVariablesEditor
-                onError={setError}
-                windowId={windowId}
-                themeId={themeId}
-                isLoading={isLoading}
-                data={data}
-              />
-            </div>
+            <ThemeVariablesEditor
+              onError={setError}
+              windowId={windowId}
+              themeId={themeId}
+              isLoading={isLoading}
+              data={data}
+            />
           </div>
           <iframe
             className={cn('uk-flex-1', { 'uk-hidden' : isLoading })}
